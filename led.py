7f1619855b71a5a7ae2f9df5cda3b14d1e1749e8
Rafaels Lasmanis <lasmanis.rafaels@gmail.com>
2019-11-05 10:14:55 +0200

    Update led.py

--- led.py
+++ led.py
@@ -3,3 +3,19 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
+
+counter = 0
+try:
+    while counter < 30:
+        print ('LED 1 on')
+        GPIO.output(18,GPIO.HIGH)
+        time.sleep(1)
+        print ('LED off')
+        GPIO.output(18,GPIO.LOW)
+        time.sleep(1)
+        print ('Kas notika')
+        counter = counter + 1
+except KeyboardInterrupt:
+    print ('\n'), counter
+finally:
+    GPIO.cleanup()
