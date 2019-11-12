38e9e64f6c7e477bd55bc9e09b732c25a2369c70
Rafaels Lasmanis <lasmanis.rafaels@gmail.com>
2019-11-12 08:48:46 +0200

    Update poga.py

--- poga.py
+++ poga.py
@@ -10,7 +10,7 @@ GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
 while True:
     button_state = GPIO.input(button)
     if button_state == GPIO.HIGH:
-        print ("HIGH")
+      print ("HIGH")
     else:
-        print ("LOW")
+      print ("LOW")
     time.sleep(0.5)       
