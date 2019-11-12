f93e8c806e47526ed373130ee6e3b25bfa4fad86
Rafaels Lasmanis <lasmanis.rafaels@gmail.com>
2019-11-12 09:31:46 +0200

    Create pogaled.py

---
+++ pogaled.py
@@ -0,0 +1,21 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+button = 4
+
+GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+
+try:
+    while True:
+         button_state = GPIO.input(button)
+         if button_state == GPIO.HIGH:
+             GPIO.output(18, True)
+             print('pods ir nospiests')
+             time.sleep(1)
+         else:
+             GPIO.output(18, False)
+except:
+    GPIO.cleanup()
