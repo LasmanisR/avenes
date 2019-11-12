4ce1a202a59faac6472b35eaa0472c70dc39618e
Rafaels Lasmanis <lasmanis.rafaels@gmail.com>
2019-11-12 10:06:51 +0200

    Update pogaled.py

--- pogaled.py
+++ pogaled.py
@@ -5,9 +5,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 button = 4
+button2 = 17
 
 GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(button2, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
 
 try:
     while True:
@@ -18,5 +21,13 @@ try:
              time.sleep(1)
          else:
              GPIO.output(18,GPIO.LOW)
+
+         button_state = GPIO.input(button2)
+         if button_state == GPIO.LOW:
+             GPIO.output(27,GPIO.HIGH)
+             print('pods v2 ir nospiests')
+             time.sleep(1)
+         else:
+             GPIO.output(27,GPIO.LOW)    
 except:
     GPIO.cleanup()
